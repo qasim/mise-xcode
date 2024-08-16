85616426addf10ee30dd3d7d2d546447ffa791fe
Qasim Iqbal <me@qas.im>
2024-08-16 00:04:14 -0400

    checklist

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mise-xcode
+
+## Checklist
+
+- [ ] Use xcodereleases.com as mapping for version to build number
+- [ ] Filter mdfind with /Applications by default
+- [ ] Set DEVELOPER_DIR in exec-env
+- [ ] Write guidance on why a tool like this doesn't try to manage Xcode download and installation, convey the by design nature of it
--- lib/utils.bash
+++ lib/utils.bash
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 set -eu -o pipefail
+
+list_all_versions() {
+    search_path="${MISE_TOOL_OPTS__SEARCH_PATH:-/}"
+
+    xcode_bundle_paths=()
+    xcode_version_aliases=()
+
+    for bundle_path in $(mdfind -onlyin "$search_path" "kMDItemCFBundleIdentifier='com.apple.dt.Xcode'"); do
+        echo "$i"
+    done
+}
+
+# sort_versions() {
+# }
