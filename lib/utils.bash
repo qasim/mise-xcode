623360354da131a25248bc06aee2b9126a602643
Qasim Iqbal <me@qas.im>
2024-09-30 14:30:58 -0400

    base implementation of required scripts

--- lib/utils.bash
+++ lib/utils.bash
@@ -4,8 +4,17 @@ set -eu -o pipefail
 
 GH_REPO="https://github.com/qasim/Xcode"
 
+fail() {
+	echo -e "mise-xcode: $*"
+	exit 1
+}
+
 list_github_tags() {
     git ls-remote --tags --refs --sort="v:refname" "$GH_REPO" | grep -o 'refs/tags/.*' | cut -d/ -f3-
+}
+
+list_github_versions() {
+    list_github_tags | cut -f1 -d"+"
 }
 
 sort_versions() {
@@ -13,11 +22,11 @@ sort_versions() {
 }
 
 list_all_versions() {
-    list_github_tags | sort_versions | xargs echo
+    list_github_versions | sort_versions | xargs echo
 }
 
 print_latest_stable_version() {
-    list_github_tags | sort_versions | tail -r | grep -m 1 -v "-"
+    list_github_versions | sort_versions | tail -r | grep -m 1 -v "-"
 }
 
 list_legacy_filenames() {
@@ -28,18 +37,45 @@ parse_legacy_file() {
     file_path="$0"
 }
 
+xcode_developer_dir() {
+    search_path="${MISE_TOOL_OPTS__SEARCH_PATH:-/}"
+
+    install_tag=$(list_github_tags | grep "$ASDF_INSTALL_VERSION+" -s || true)
+    if [ -z "$install_tag" ]; then
+        fail "No Xcode version exists that corresponds to $ASDF_INSTALL_VERSION."
+    fi
+
+    install_build=$(echo "$install_tag" | cut -d"+" -f2)
+
+    install_developer_dir=""
+    for bundle_path in $(mdfind -onlyin "$search_path" "kMDItemCFBundleIdentifier='com.apple.dt.Xcode'"); do
+        version_plist_path="$bundle_path/Contents/version.plist"
+        build=$(/usr/libexec/PlistBuddy -c "print 'ProductBuildVersion'" "$version_plist_path")
+        if [ "$install_build" == "$build" ]; then
+            install_developer_dir="$bundle_path/Contents/Developer"
+        fi
+    done
+
+    if [ -z "$install_developer_dir" ]; then
+        fail "Xcode version $ASDF_INSTALL_VERSION not found on disk."
+    fi
+
+    echo "$install_developer_dir"
+}
+
+download() {
+    if [ "$ASDF_INSTALL_TYPE" != "version" ]; then
+        fail "Git ref install types are not supported."
+    fi
+
+    xcode_developer_dir 1> /dev/null
+}
+
 install() {
-    # search_path="${MISE_TOOL_OPTS__SEARCH_PATH:-/}"
-
-    # xcode_bundle_paths=()
-    # xcode_version_aliases=()
-
-    # for bundle_path in $(mdfind -onlyin "$search_path" "kMDItemCFBundleIdentifier='com.apple.dt.Xcode'"); do
-    #     echo "$bundle_path"
-    # done
-    echo "ok"
+    xcode_developer_dir 1> /dev/null
 }
 
 prepare_environment() {
-    echo "ok"
+    DEVELOPER_DIR=$(xcode_developer_dir)
+    export DEVELOPER_DIR
 }
