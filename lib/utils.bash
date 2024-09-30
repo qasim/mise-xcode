3a3f678b931408fdd6d7c0e2aaab4be9010d2792
Qasim Iqbal <me@qas.im>
2024-09-30 12:14:11 -0400

    listing versions

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,18 +2,22 @@
 
 set -eu -o pipefail
 
+GH_REPO="https://github.com/qasim/Xcode"
+
+list_github_tags() {
+    git ls-remote --tags --refs --sort="v:refname" "$GH_REPO" | grep -o 'refs/tags/.*' | cut -d/ -f3-
+}
+
+sort_versions() {
+    sed -e '/[a-zA-Z]/! s/$/|/' | sort -V | sed -e 's/|$//'
+}
+
 list_all_versions() {
-    search_path="${MISE_TOOL_OPTS__SEARCH_PATH:-/}"
-
-    xcode_bundle_paths=()
-    xcode_version_aliases=()
-
-    for bundle_path in $(mdfind -onlyin "$search_path" "kMDItemCFBundleIdentifier='com.apple.dt.Xcode'"); do
-        echo "$bundle_path"
-    done
+    list_github_tags | sort_versions | xargs echo
 }
 
 print_latest_stable_version() {
+    list_github_tags | sort_versions | tail -r | grep -m 1 -v "-"
 }
 
 list_legacy_filenames() {
@@ -25,8 +29,17 @@ parse_legacy_file() {
 }
 
 install() {
+    # search_path="${MISE_TOOL_OPTS__SEARCH_PATH:-/}"
+
+    # xcode_bundle_paths=()
+    # xcode_version_aliases=()
+
+    # for bundle_path in $(mdfind -onlyin "$search_path" "kMDItemCFBundleIdentifier='com.apple.dt.Xcode'"); do
+    #     echo "$bundle_path"
+    # done
+    echo "ok"
 }
 
 prepare_environment() {
-
+    echo "ok"
 }
